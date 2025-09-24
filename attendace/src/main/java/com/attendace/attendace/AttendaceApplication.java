99e16c2723fc7cf1d7bf964bb706e04a71c8c5ad
romayerm <kseniya.yermakova97@bcmail.cuny.edu>
2025-09-23 23:04:25 -0400

    Environment Set Up
    Created a Maven project and connected it to the database (still empty for now).

---
+++ attendace/src/main/java/com/attendace/attendace/AttendaceApplication.java
@@ -0,0 +1,13 @@
+package com.attendace.attendace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendaceApplication.class, args);
+	}
+
+}
---
+++ attendace/src/test/java/com/attendace/attendace/AttendaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.attendace.attendace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
